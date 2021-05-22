0676f18649070bb6ce2c590a9c853cd4e63ef9d1
mtpullin <mtpullin@gmail.com>
2021-05-22 08:49:41 -0400

    adding form inputs

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 var currentDay = document.getElementById("currentDay")
-var taskEl = document.getElementById("#task")
+var taskEl = document.getElementById("task")
 
 currentDay.innerHTML = moment().format("MMM, DD, YYYY")
 
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    if(!tasks){
+    tasks = []
+    }
+};
+
+
+
+
+
+$("taskEl").on("click",function() {
+    var text = $(this)
+    .text()
+    .trim()
+
+    var textInput = $("<textarea>").addClass("form-control").val(text);
+    $(this).replaceWith(textInput);
+
+    textInput.trigger("focus");
+});
